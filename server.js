ffe46dd0269302dbd2ac17fe5977b712a80a4115
Vinicius Bezerra <viniciusg21bezerra@gmail.com>
2025-10-07 18:03:03 -0300

    Iniciando com o CRUD

---
+++ server.js
@@ -0,0 +1,13 @@
+import app from "./src/app.js";
+
+const PORT = 3000;
+
+const rotas = {
+    '/': 'Curso de Node.js',
+    '/livros': 'Entrei na rota de livros',
+    '/autores': 'Entrei na rota de autores',
+}
+
+app.listen(PORT, () => {
+    console.log("Servidor escutando!");
+});
